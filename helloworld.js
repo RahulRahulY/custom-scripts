939d4daa627e49375b8fb43ea22791043a0ad095
RahulRahulY <rahulyamali123.com>
2026-03-12 23:29:48 +0530

    log-n5

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,12 @@
 UI.onEvent(function (eventType, data) {
   UI.log("Event type: " + eventType);
   UI.log("Event data: " + JSON.stringify(data));
+
+  if (eventType === "uiAction") {
+    if (data.id === "customButton") {
+      UI.alert("Custom button clicked!");
+    }
+  }
 });
 
 var html =
