881ba8881b5040479d6ca39c4471992b47d784ec
RahulRahulY <rahulyamali123.com>
2026-03-13 10:04:58 +0530

    idk

--- helloworld.js
+++ helloworld.js
@@ -2,11 +2,11 @@ UI.onEvent(function (eventType, data) {
   UI.log("Event type: " + eventType);
   UI.log("Event data: " + JSON.stringify(data));
 
-  if (eventType === "uiAction") {
-    if (data && data.id === "customButton") {
-      UI.alert("Custom button clicked!");
-    }
-  }
+//   if (eventType === "uiAction") {
+//     if (data && data.id === "customButton") {
+//       UI.alert("Custom button clicked!");
+//     }
+//   }
 });
 
 var html =
