79e2464a9ae4cde4b4341cb02e252fb26b2e2e76
RahulRahulY <rahulyamali123.com>
2026-03-17 15:49:57 +0530

    log entity

--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,10 @@
 var html =
   "<div style='width:100%;height:100%;border:1px solid rgb(235, 235, 235);background-color: white;border-radius:2px; display:flex;flex-direction:column;'>\n" +
   "    <div style='font-family: Arial, sans-serif; font-size: 13px; color: rgb(51, 51, 51); font-weight: bold; width: 100%; height: 18px;'>\n" +
-  "            Static Page\n" +
+  "            EM\n" +
   "        </div>\n" +
   "    <div style='flex:1;padding: 0px 10px 10px;overflow:auto'>\n" +
-  "        <p>Welcome to the Static Page</p>\n" +
+  "        <p>EM Actions</p>\n" +
   "        <button id='customButton' style='padding: 10px 20px; background-color: #007bff; color: white; border: none; border-radius: 4px; cursor: pointer; font-size: 14px;'>Click Me</button>\n" +
   "    </div>\n" +
   "</div>";
@@ -22,5 +22,9 @@ UI.onEvent(function (eventType, data) {
     UI.openSearch({});
 
     UI.log("Button clicked!");
+
+    UI.getEntity().then(function (entity) {
+      UI.log("Current Entity: ", entity);
+    });
   }
 });
