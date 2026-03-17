978b43680d648c168f77c5380f74ab29c13bc909
RahulRahulY <rahulyamali123.com>
2026-03-17 18:03:26 +0530

    log-n16

--- helloworld.js
+++ helloworld.js
@@ -1,17 +1,13 @@
 var html =
-  "<div style='width:100%;height:100%;border:1px solid rgb(235, 235, 235);background-color: white;border-radius:2px; display:flex;flex-direction:column;'>\n" +
-  "    <div style='font-family: Arial, sans-serif; font-size: 13px; color: rgb(51, 51, 51); font-weight: bold; width: 100%; height: 18px;'>\n" +
-  "            EM\n" +
-  "        </div>\n" +
-  "    <div style='flex:1;padding: 0px 10px 10px;overflow:auto'>\n" +
-  "        <p>EM Actions</p>\n" +
+  "<div style='width:100%;height:100%;border:1px solid rgb(235, 235, 235);background-color: white;border-radius:2px;'>\n" +
+  "    <div style='padding: 0px 10px 10px;overflow:auto'>\n" +
+  "        <p>EM</p>\n" +
   "        <button id='customButton' ui-actions='click' style='padding: 10px 20px; background-color: #007bff; color: white; border: none; border-radius: 4px; cursor: pointer; font-size: 14px;'>Click Me</button>\n" +
   "    </div>\n" +
   "</div>";
 
 UI.setHtml(html);
 
-// Get all entity and context related details
 UI.getEntity().then(function (entity) {
   UI.log("Entity: " + JSON.stringify(entity));
 });
