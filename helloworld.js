39c12582059ca330d3dd51a5f06bec1193eb85aa
RahulRahulY <rahulyamali123.com>
2026-03-18 16:39:35 +0530

    css-2

--- helloworld.js
+++ helloworld.js
@@ -1,14 +1,16 @@
 let html =
-  "<div style='width:100%;height:100%;border:1px solid rgb(235, 235, 235);background-color: white;border-radius:2px; display:flex;flex-direction:colum'>\n" +
+  "<div style='border:1px solid rgb(235, 235, 235);background-color: white;border-radius:2px; display:flex;flex-direction:column'>\n" +
   "    <div style='padding: 0px 10px 10px;overflow:auto'>\n" +
-  "        <p>EM Integration</p>\n" +
+  "        <p style='font-size: 18px; font-weight: bold; margin: 10px 0;'>EM Integration</p>\n" +
 
   "        <div style='margin: 10px 0;'>\n" +
-  "            <p>Enity Name: <span id='entityName'>-</span></p>\n" +
-  "            <p>Crosswalks Count: <span id='crosswalkCount'>-</span></p>\n" +
+  "            <p style='font-size: 14px; color: #666;'>Enity Name: <span id='entityName' style='color: #333; font-weight: bold;'>-</span></p>\n" +
+  "            <p style='font-size: 14px; color: #666;'>Crosswalks Count: <span id='crosswalkCount' style='color: #333; font-weight: bold;'>-</span></p>\n" +
   "        </div>\n" +
   "    </div>\n" +
-  "    <button id='customButton' ui-actions='click' style='padding: 10px 20px; background-color: #007bff; color: white; border: none; border-radius: 4px; cursor: pointer; font-size: 14px;'>Button</button>\n" +
+  "    <div style='padding: 10px;'>\n" +
+  "        <button id='customButton' ui-actions='click' style='padding: 10px 20px; background-color: #007bff; color: white; border: none; border-radius: 4px; cursor: pointer; font-size: 14px;'>Button</button>\n" +
+  "    </div>\n" +
   "</div>";
 
 UI.setHtml(html);
