770d443be8810acab6e8dd511fed97e959cda077
RahulRahulY <rahulyamali123.com>
2026-03-18 19:53:51 +0530

    box-shadow-2 and fetch on click

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,5 @@
 let html =
-  "<div style='background-color: white;display:flex;flex-direction:column;box-shadow:0 4px 6px rgba(0,0,0,0.1)'>\n" +
+  "<div style='background-color: white;display:flex;flex-direction:column;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);'>\n" +
   "    <div style='padding: 0px 10px 10px;overflow:auto'>\n" +
   "        <p style='font-size: 18px; font-weight: 400; margin: 10px 0;'>EM Integration</p>\n" +
   "        <div style='margin: 10px 0;'>\n" +
@@ -7,7 +7,7 @@ let html =
   "            <p style='font-size: 14px; color: #666;'>Enity Name: <span id='entityName' style='color: #333;'>{0}</span></p>\n" +
   "        </div>\n" +
   "    </div>\n" +
-  "    <div style='padding: 10px;'>\n" +
+  "    <div style='padding: 10px; display: flex; justify-content: flex-end;'>\n" +
   "        <button id='customButton' ui-actions='click' style='padding: 10px 20px; background-color: #007bff; color: white; border: none; border-radius: 4px; cursor: pointer; font-size: 14px;'>Button</button>\n" +
   "    </div>\n" +
   "</div>";
@@ -43,22 +43,14 @@ UI.onEvent(function (eventType, data) {
     data.type === "click"
   ) {
     UI.log("Button clicked!");
-  }
-});
 
-Promise.all([
-  UI.getApiPath(),
-  UI.getTenant(),
-  UI.getUiConfiguration(),
-  UI.getEntity(),
-]).then(function (values) {
-  let config = values[2];
-  let entity = values[3];
-  let [,entityId] = entity.uri.split("/");
-  
-  UI.setHtml(
-    html
-      .replace("{0}", entity.label)
-      .replace("{1}", entityId),
-  );
+    UI.getEntity().then(function (entity) {
+      let [, entityId] = entity.uri.split("/");
+      UI.setHtml(
+        html
+          .replace("{0}", entity.label)
+          .replace("{1}", entityId)
+      );
+    });
+  }
 });
