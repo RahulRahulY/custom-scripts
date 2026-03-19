ba6cbc8f3771dc5500b63dced7b8754b252090e3
RahulRahulY <rahulyamali123.com>
2026-03-19 21:13:43 +0530

    link-change-22

--- helloworld.js
+++ helloworld.js
@@ -34,7 +34,7 @@ UI.onEvent(function (eventType, data) {
 });
 
 UI.api(
-  "https://gist.githubusercontent.com/RahulRahulY/beb746f99877538f6e67cb1a37e01f32/raw/74417098ca5d8e593f6cae538b5259e093a0220c/test.json",
+  "https://raw.githubusercontent.com/RahulRahulY/custom-scripts/refs/heads/main/test.json",
   "GET",
 )
   .then(function (response) {
