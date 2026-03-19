f7656e941204b166a88fde3b589227f628723ecb
RahulRahulY <rahulyamali123.com>
2026-03-19 16:27:02 +0530

    header

--- helloworld.js
+++ helloworld.js
@@ -2,10 +2,10 @@ let html =
   "<div style='background-color: white;display:flex;flex-direction:column;width:100%;height:100%;box-sizing:border-box;border-radius:4px;margin:0;overflow:hidden;box-shadow: inset 0 0 1px 2px rgba(0, 0, 0, 0.05)'>\n" +
   
   "    <div style='padding: 8px 0px 12px 14px;overflow:auto;flex:1 1 auto;box-sizing:border-box'>\n" +
-  "        <p style='font-size: 18px; font-weight: 400; margin: 10px 0;'>EM Integration</p>\n" +
+  "        <p style='font-size: 18px; font-weight: 400; margin: 10px 0;'>Entity Details</p>\n" +
   "        <div style='margin: 10px 0;'>\n" +
-  "            <p style='font-size: 14px; color: #666;'>Entity Id: <span id='crosswalkCount' style='color: #333;'></span></p>\n" +
-  "            <p style='font-size: 14px; color: #666;'>Enity Name: <span id='entityName' style='color: #333;'></span></p>\n" +
+  "            <p style='font-size: 14px; color: #666;'>Entity Id: <span id='entityId' style='color: #333;'></span></p>\n" +
+  "            <p style='font-size: 14px; color: #666;'>Entity Name: <span id='entityName' style='color: #333;'></span></p>\n" +
   "        </div>\n" +
   "    </div>\n" +
   "    <div style='padding: 10px; display: flex; justify-content: flex-end;'>\n" +
@@ -49,7 +49,7 @@ UI.onEvent(function (eventType, data) {
       let [, entityId] = entity.uri.split("/");
 
       UI.setChildHtml("entityName", entity.label);
-      UI.setChildHtml("crosswalkCount", entityId);
+      UI.setChildHtml("entityId", entityId);
     });
   }
 });
